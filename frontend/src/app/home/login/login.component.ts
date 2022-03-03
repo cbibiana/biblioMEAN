b74c5cca473a8fab778c48d1dabd1311d9573a8b
cbibiana <cbibiana.sepulveda@udea.edu.co>
2022-03-03 16:43:56 -0500

    Se agrega login

--- frontend/src/app/home/login/login.component.ts
+++ frontend/src/app/home/login/login.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  MatSnackBar,
+  MatSnackBarHorizontalPosition,
+  MatSnackBarVerticalPosition,
+} from '@angular/material/snack-bar';
+import { Router } from '@angular/router';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-login',
@@ -6,8 +13,48 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  loginData: any;
+  message: string = '';
+  horizontalPosition: MatSnackBarHorizontalPosition = 'end';
+  verticalPosition: MatSnackBarVerticalPosition = 'top';
+  durationInSeconds: number = 2000;
+  constructor(
+    private _userService: UserService,
+    private _router: Router,
+    private _snackBar: MatSnackBar
+  ) {
+    this.loginData = {};
+  }
 
-  constructor() { }
+  login() {
+    if (
+      !this.loginData.email ||
+      !this.loginData.password
+    ) {
+      this.message = 'Incompletos datos';
+      this.openSnackBarError();
+    } else {
+      this._userService.login(this.loginData).subscribe({
+        next: (v) => {
+          localStorage.setItem('token', v.token);
+          this._router.navigate(['/listBook']);
+        },
+        error: (e) => {
+          this.message = e.error.message;
+          this.openSnackBarError();
+        },
+      });
+    }
+  }
+
+  openSnackBarError() {
+    this._snackBar.open(this.message, 'X', {
+      horizontalPosition: this.horizontalPosition,
+      verticalPosition: this.verticalPosition,
+      duration: this.durationInSeconds,
+      panelClass: ['styleSnackBarError'],
+    }); 
+  }
 
   ngOnInit(): void {
   }
--- frontend/src/app/services/user.service.ts
+++ frontend/src/app/services/user.service.ts
@@ -14,4 +14,7 @@ export class UserService {
   registerUser(user: any) {
     return this._http.post<any>(this.env + 'user/registerUser', user);
   }
+  login(user: any) {
+    return this._http.post<any>(this.env + 'user/login', user);
+  }
 }
